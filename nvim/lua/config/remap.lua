024504ed4adf0921ebc598039ca4678562af002e
Sen <s3nfrancis@gmail.com>
2025-07-10 16:53:02 -0500

    remapping undo :)

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -14,3 +14,6 @@ vim.keymap.set("i", "<Up>", "<nop>")
 vim.keymap.set("i", "<Down>", "<nop>")
 vim.keymap.set("i", "<Left>", "<nop>")
 vim.keymap.set("i", "<Right>", "<nop>")
+
+vim.keymap.set("n", "<C-u>", "u")
+vim.keymap.set("n", "u", "<nop>")
